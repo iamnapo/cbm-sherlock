929d027bb45cf6320e5247b5339007856e67b900
Napoleon-Christos Oikonomou <Napoleonoikon@gmail.com>
2017-11-21 21:11:52 +0200

    pure js implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cbm-sherlock
-A script that uses the CallByMeaning project to deduce(🕵🏼) the number of characters that appear in the book The Adventures of Sherlock Holmes.
+
+A script that uses the CallByMeaning project to deduce(🕵🏼) the number of characters that appear in the book The Adventures of Sherlock Holmes: A scandal in Bohemia.
---
+++ pure-sherlock.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+const natural = require('natural');
+const tokenizer = new natural.WordPunctTokenizer();
+
+function main() {
+  //  0. Read the file
+  const sherlockFile = fs.readFileSync('./lib/sherlock.txt', 'utf8');
+
+  //  1. Create an array containing every word
+  let sherlock = tokenizer.tokenize(sherlockFile.replace(/(\r\n|\n)/gm, ' ').replace(/(\r)/gm, ''));
+
+  //  2. Remove unnecessary words
+  //    a. Join concecutive words that start with Uppercase i.e ['Sherlock', 'Holmes'] -> ['Sherlock Holmes']
+  const upperCase = new RegExp(/^[A-Z]/);
+  const punctuation = new RegExp(/^['!"#$%&\\'()*+,\-./:;<=>?@[\\\]^_`{|}~']/ );
+  for (let i = 0; i < sherlock.length; i++) {
+    if (upperCase.test(sherlock[i]) && !punctuation.test(sherlock[i])) {
+      if (upperCase.test(sherlock[i + 1]) && !punctuation.test(sherlock[i + 1])) {
+        sherlock[i] += ' '.concat(sherlock[i + 1]);
+        sherlock[i + 1] = sherlock[i + 1].toLowerCase();
+        if (upperCase.test(sherlock[i + 2]) && !punctuation.test(sherlock[i + 2])) {
+          sherlock[i] += ' '.concat(sherlock[i + 2]);
+          sherlock[i + 2] = sherlock[i + 2].toLowerCase();
+        }
+      }
+    }
+  }
+  //    b. Remove words that don't start with an uppercase letter but keep periods
+  sherlock = sherlock.filter((word) => (upperCase.test(word) || punctuation.test(word)) && word !== 'I'); // I is a special case
+
+  //  3. Remove words that are after a punctuation point (and also the point itself)
+  let temp = [];
+  for (let i = 0; i < sherlock.length; i++) {
+    if (punctuation.test(sherlock[i])) continue;
+    if (punctuation.test(sherlock[i - 1])) continue;
+    temp.push(sherlock[i]);
+  }
+  sherlock = temp;
+
+  //  4. Remove words that are in ALL CAPS
+  sherlock = sherlock.filter((word) => word !== word.toUpperCase());
+
+  //  5. Remove dublicates
+  //    a. Things that appear more than once
+  sherlock = [...new Set(sherlock)];
+
+  //    b. Things that are the same i.e 'Holmes', 'Sherlock', 'Sherlock Holmes'
+  sherlock = sherlock.filter((w, i, a) => {
+    for (let word of a) {
+      if (word.indexOf(w) > -1 && word !== w) return word.length < w.length;
+    }
+    return true;
+  });
+
+  //  6. Write them to a file
+  try {
+    fs.writeFileSync(__dirname.concat('/results/pure.txt'), sherlock, ['utf8', 0o666, 'w+']);
+    return 'Done!';
+  } catch (error) {
+    return toString(error);
+  }
+}
+console.log(main());
